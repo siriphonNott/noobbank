ecdb12cd89ae2266db4c9201207b404aed91d082
DESKTOP-CCARM5B\siriphonnot <siriphon@ucconnect.co.th>
2018-03-25 21:48:31 +0700

    fix pdo set utf 8

--- app/model/Database.php
+++ app/model/Database.php
@@ -13,7 +13,7 @@ class Database
         $servername = "localhost";
 
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
